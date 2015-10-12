cb81c5f0e89583e8a442007903ca1db3aea3b4ad
Duke <dukekhaos@gmail.com>
2015-10-11 22:00:30 -0300

    send notification to google cloud messaging

--- lib/gcmex.ex
+++ lib/gcmex.ex
@@ -1,2 +1,31 @@
 defmodule GcmEx do
+  @endpoint "https://gcm-http.googleapis.com"
+
+  alias GcmEx.Message
+
+  def send(message = %Message{}, http \\ HTTPoison) do
+    post(http, "/gcm/send", Poison.encode!(message), Application.get_env(:gcmex, :api_key))
+  end
+
+  defp post(http, path, data, key) do
+    headers = [
+      {"Content-Type", "application/json"},
+      {"Authorization", "key=" <> key}
+    ]
+    case http.post process_url(path), data, headers do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, process_response_body(body)}
+      _ -> {:error}
+    end
+  end
+
+  defp process_url(path) do
+    @endpoint <> path
+  end
+
+  defp process_response_body(body) do
+    body
+    |> Poison.decode!
+    |> Enum.reduce(%{}, fn {k, v}, map -> Dict.put(map, String.to_atom(k), v) end)
+  end
 end
--- test/gcmex_test.exs
+++ test/gcmex_test.exs
@@ -1,8 +1,21 @@
-defmodule GcmExTest do
+defmodule Bakisto.GCMTest do
   use ExUnit.Case
-  doctest GcmEx
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias GcmEx
+  alias GcmEx.Message
+
+  test "send to GSM" do
+    defmodule HTTPtest do
+      def post(path, data, headers) do
+        assert path == "https://gcm-http.googleapis.com/gcm/send"
+        assert data == "{\"to\":\"user1\",\"time_to_live\":null,\"restricted_package_name\":null,\"registration_ids\":null,\"priority\":null,\"notification\":null,\"dry_run\":null,\"delay_while_idle\":null,\"data\":null,\"content_available\":null,\"collapse_key\":null}"
+        assert headers == [{"Content-Type", "application/json"}, {"Authorization", "key=xxxtestxxx"}]
+        {:ok, %HTTPoison.Response{status_code: 200, body: "{\"multicast_id\":7392464421346996000,\"success\":1,\"failure\":0,\"canonical_ids\":0,\"results\":[{\"message_id\":\"0:1442091036154095%251338c2f9fd7ecd\"}]}"}}
+      end
+    end
+
+    {:ok, response} = GcmEx.send(%Message{to: "user1"}, HTTPtest)
+    assert 7392464421346996000 == response.multicast_id
+    assert List.first(response.results)["message_id"] == "0:1442091036154095%251338c2f9fd7ecd"
   end
 end
