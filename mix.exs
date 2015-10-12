85ba77c4953348d0d2f760e325a2db9b5740fe6b
Duke <dukekhaos@gmail.com>
2015-10-11 23:05:00 -0300

    add package files

--- mix.exs
+++ mix.exs
@@ -5,13 +5,22 @@ defmodule Gcmex.Mixfile do
     [app: :gcmex,
      version: "0.0.1",
      elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: "Google Cloud Messaging client library for elixir",
+     package: package,
      deps: deps]
   end
 
   def application do
     [applications: [:logger]]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Duke"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/dukex/gcmex"}
+    ]
   end
 
   defp deps do
